b0acb3236d19d54a471df6fc7c1da7c546fecc16
Nilesh Kumar Yadav <contact.nileshy@gmail.com>
2025-04-05 12:34:33 +0530

    project init

---
+++ lib/config.ts
@@ -0,0 +1,30 @@
+// lib/config.ts
+
+const AppConfig = {
+    title: "Editabled",
+    description: "Platform for creative Notions.",
+    // logo: "/images/logo.png",
+    // icon: "/images/favicon.ico",
+  
+    colors: {
+      primary: "#0D6EFD",
+      secondary: "#6C757D",
+      background: "#F8F9FA",
+      text: "#212529",
+    },
+  
+    meta: {
+    //   ogImage: "/images/og-image.png",
+      twitterHandle: "@nileshyadavme",
+    },
+  
+    links: {
+      github: "https://github.com/nileshyadavme/editabled",
+    //   docs: "https://myapp.dev/docs",
+    },
+  
+    author: "Nilesh Kumar Yadav",
+  };
+  
+  export default AppConfig;
+  
